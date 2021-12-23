ca25440defa5191e3391bf49f0a9746ebf88e56c
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-23 21:43:01 +0530

    added programme that prints wether the entered number is odd or even

---
+++ Evenodd.java
@@ -0,0 +1,10 @@
+class Evenodd{
+	public static void main(String[]args){
+		byte number=11;
+		if(number %2==0){
+			System.out.println("The number is even");
+		}else{
+			System.out.println("The number is odd");
+		}
+	}
+}
