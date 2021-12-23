8502c7f86209f95bae33cc9cb1fd090801c1e1a7
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-23 21:44:20 +0530

    added programme that prints the even numbers between entered number

---
+++ Evenno.java
@@ -0,0 +1,10 @@
+class Evenno{
+	public static void main (String [] args){
+		byte number=12;
+			for (int num=1 ; num<=number ; num++ ){
+				if (num%2==0){
+					System.out.println(num);
+				}
+			}
+	}	
+}
